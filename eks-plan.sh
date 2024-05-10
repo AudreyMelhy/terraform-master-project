596a582a0fa079bdf98bea5404c7f498f4202784
AudreyMelhy <audreybsl12@gmail.com>
2024-05-10 10:51:04 +0300

    removed eks-plan.sh

--- eks-plan.sh
+++
@@ -1,8 +0,0 @@
-echo "*****************************************************************"
-echo "Deploying the eks-control-plane module"
-echo "*****************************************************************"
-cd terraform/resources/eks-control-plane
-terraform init
-terraform fmt
-terraform plan
-cd -
